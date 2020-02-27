d133f3556b90bd161e4defb61bf1b13da60398a5
h4x0rMadness <robertmorrislike@gmail.com>
2020-02-26 22:58:15 -0500

    Add files via upload

--- test_DailyFeed.py
+++ test_DailyFeed.py
@@ -173,8 +173,9 @@ class DailyFeed:
 
 def test_method():
     video_existence = os.path.exists("./dailyfeed.mov")
+    pic_existence = os.path.exists("./0.png")
     assert video_existence == True
-   
+    assert pic_existence == False
 
 if __name__ == '__main__':
     if os.path.exists("./keys"):
